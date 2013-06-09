444b96a1ad334708c7a87a988226cc4fc0b1bd71
David Handel <handeld@gmail.com>
2013-06-09 15:11:25 -0400

    Modal dialog added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function() {
 	var founders = [{lastName: 'Handel', firstName: 'David', age: 60, id: 0}, 
 		{lastName: 'Mazin', firstName: 'Lev', age: 33, id: 1},{lastName: 'Smallwood', firstName: 'April', age: 47, id: 2}];	
-	
+	$('.modal').hide();
 	function addNewOptions(array) {
 		var length = array.length;
 		for (i = 0; i< length; i++) {
@@ -41,7 +41,22 @@ $(function() {
 	  		$('.age').text("");  
 	  	} 	
 	});
-	
+	 var $close = $('.close')
+	$close.mouseover(function () {
+		$close.css("cursor", "pointer");
+	} );
+	$close.click(function () {
+		// $('.modal').attr("hidden", "true");
+		$('.modal').toggle();
+		$(".page").css({ opacity: 1.0 });
+		$(".showmodal").attr('disabled', false);
+	})
+	$(".showModal").click(function() {
+		$('.modal').toggle();
+		$close.attr("disabled");
+		$(".page").css({ opacity: 0.3 });
+		$(".showmodal").attr('disabled', true);
+	})
 });
 
 
